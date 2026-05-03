a28f7910b894738893c315171e79d7c29de69789
vitanh1703 <diema448@gmail.com>
2026-05-03 23:16:10 +0700

    update cart

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,5 @@
 <?php
-define( 'WP_CACHE', true );
+
 
 /**
  * The base configuration for WordPress
---
+++ wp-content/languages/loco/themes/astra-vi.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'Astra 4.13.1','report-msgid-bugs-to'=>'https://wordpress.org/support/theme/astra','last-translator'=>'','language-team'=>'Tiếng Việt','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2026-04-24T12:15:55+00:00','po-revision-date'=>'2026-05-03 14:53+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'astra
+','language'=>'vi','plural-forms'=>'nplurals=1; plural=0;','x-loco-version'=>'2.8.4; wp-6.9.4; php-8.2.12','messages'=>[]];
